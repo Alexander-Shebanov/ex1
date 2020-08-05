7594144d36ab34c6475b41bf3499fb1d5483bd7a
Alexander-Shebanov <legionqq996@gmail.com>
2020-08-05 11:26:33 +0300

    asdad

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("header__nav-container").classList.toggle("show");
+}
+
+function hoverFunction() {
+    console.log(document.getElementsByClassName("select-block__text"))
+    document.getElementsByClassName("select-block__text").classList.toggle("hover-color");
 }
